4c4328172855b02787cb3722f81ee1c0c10c8e21
mhoysj <mhoyosj2@eafit.edu.co>
2024-08-15 10:18:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from PIL import Image
 image = Image.open('Taylor.png')
-texto= st.text_input('Escribe algo', 'Este es mi texto')
+
 
 st.image(image, caption='Taylor Swift')
 
 st.title("Amo a Taylor Swift")
 st.header("Top 10 personas obsesionadas con Taylor Swift")
 st.write("1. Yo, Manuela Hoyos Jimènez.")
+texto= st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es' , texto)
