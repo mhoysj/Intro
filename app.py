f6ea8caa645b85a53d25f36a8d95e2c96a4d0804
mhoysj <mhoyosj2@eafit.edu.co>
2024-08-15 10:13:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ image = Image.open('Taylor.png')
 st.image(image, caption='Taylor Swift')
 
 st.title("Amo a Taylor Swift")
-st.header("Top 10 personas obesesionadas con Taylor Swift")
+st.header("Top 10 personas obsesionadas con Taylor Swift")
 st.write("1. Yo, Manuela Hoyos Jimènez.")
