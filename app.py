fc9f2b9dfb0c46093e4eee6b920c748c6d42e381
mhoysj <mhoyosj2@eafit.edu.co>
2024-08-15 10:10:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open('Taylor Swift.webp')
+image = Image.open('Taylor')
+st.image(image, caption='Taylor Swift')
+
 st.title("Amo a Taylor Swift")
 st.header("Top 10 personas obesesionadas con Taylor Swift")
 st.write("1. Yo, Manuela Hoyos Jimènez.")
